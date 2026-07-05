13cb665aff459915a35384e728f1af69145094b9
Iván Fernando Hernández Ortiz <ortizfernando05@gmail.com>
2026-07-05 15:24:07 -0600

    refactor: remove unused React import from SpotlightEffect component

--- src/core/presentation/components/SpotlightEffect.tsx
+++ src/core/presentation/components/SpotlightEffect.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import React, { useEffect, useState } from "react";
+import { useEffect, useState } from "react";
 
 export function SpotlightEffect() {
   const [position, setPosition] = useState({ x: -1000, y: -1000 });
